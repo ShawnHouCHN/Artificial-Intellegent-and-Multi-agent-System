6766d970ae99e14d9c9c84fbce8788f1ed2eaed0
s134104 <mohammad@mohammads-mbp-4.eduroam.wireless.dtu.dk>
2017-02-07 15:05:22 +0100

    First revision

---
+++ Mohammad/Assignment1/src/searchclient/searchclient/Heuristic.java
@@ -0,0 +1,124 @@
+package searchclient;
+
+import java.util.Comparator;
+
+import searchclient.NotImplementedException;
+
+public abstract class Heuristic implements Comparator<Node> {
+	
+	public Node initialState;
+	
+	public Heuristic(Node initialState) {
+		// Here's a chance to pre-process the static parts of the level.
+
+		this.initialState = initialState;
+	}
+
+	public int h(Node n) {
+		// Implementing Manhattan Distance:
+		/*
+		URL: http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
+
+			function heuristic(node) =
+    			dx = abs(node.x - goal.x)
+    			dy = abs(node.y - goal.y)
+    			return D * (dx + dy)
+		*/
+		
+		int totalDistance = 0;
+		int minDist = 0;
+		
+		// Get box
+		for(int i = 1; i < n.MAX_ROW - 1; i++) {
+			// Get row
+			for(int j = 1; j < n.MAX_COL - 1; j++) {
+				// Get column
+				
+				char box = n.boxes[i][j];
+				
+				if('A' <= box && box <= 'Z') {
+					minDist = Integer.MAX_VALUE;
+					
+					box = Character.toLowerCase(box);
+					
+					for(int k = 1; k < n.MAX_ROW - 1; k++) {
+						for(int l = 1; l < n.MAX_COL - 1; l++) {
+							char goal = n.goals[k][l];
+							if('a' <= goal && goal <= 'z' && box == goal) {
+								int row = i - k;
+								int col = j - l;
+								
+								minDist = Math.min((Math.abs(row) + Math.abs(col)), minDist);
+							}
+						}
+					}
+					
+					totalDistance += minDist;
+				}
+			}
+		}
+
+    	//int dx = Math.abs(n.agentRow - getPositionX);
+    	//int dy = Math.abs(n.agentCol - getPositionY);
+		
+		return totalDistance;
+	}
+
+	public abstract int f(Node n);
+
+	@Override
+	public int compare(Node n1, Node n2) {
+		return f(n1) - f(n2);
+	}
+
+	public static class AStar extends Heuristic {
+		public AStar(Node initialState) {
+			super(initialState);
+		}
+
+		@Override
+		public int f(Node n) {
+			return n.g() + this.h(n);
+		}
+
+		@Override
+		public String toString() {
+			return "A* evaluation";
+		}
+	}
+
+	public static class WeightedAStar extends Heuristic {
+		private int W;
+
+		public WeightedAStar(Node initialState, int W) {
+			super(initialState);
+			this.W = W;
+		}
+
+		@Override
+		public int f(Node n) {
+			return n.g() + this.W * this.h(n);
+		}
+
+		@Override
+		public String toString() {
+			return String.format("WA*(%d) evaluation", this.W);
+		}
+	}
+
+	public static class Greedy extends Heuristic {
+		public Greedy(Node initialState) {
+			super(initialState);
+		}
+
+		@Override
+		public int f(Node n) {
+			return this.h(n);
+		}
+
+		@Override
+		public String toString() {
+			return "Greedy evaluation";
+		}
+	}
+}
