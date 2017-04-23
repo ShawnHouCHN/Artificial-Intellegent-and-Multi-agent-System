af21232480d488e91e86061f9f03c8dbf35bae10
MA <s134104@student.dtu.dk>
2017-04-23 10:03:58 +0200

    computePaths() & detectConflicts()
    .

---
+++ Mohammad/Final project/Merged/MergedProject/src/sampleclients/Agent.java
@@ -0,0 +1,308 @@
+package sampleclients;
+
+import java.awt.Point;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import sampleclients.Heuristic.AStar;
+import sampleclients.RandomWalkClient;
+import sampleclients.Strategy.StrategyBestFirst;
+
+//agent class
+
+/*****************************************************************************/
+public class Agent {
+	Box currentBox;
+	Goal currentGoal;
+	char id;
+	String color;
+	int[] location;
+	public HashMap<Point, Goal > myGoals = new HashMap<Point, Goal >();
+	public HashMap<Point, Box > myBoxes = new HashMap<Point, Box >();
+	public LinkedList<Command> plan=new LinkedList<Command>();
+	public LinkedList<Node> solution=new LinkedList<Node>();
+	public Strategy strategy;
+	public Node initialState;
+	
+	public Agent( char id, String color , int[] location) {
+		System.err.println("Found " + color + " agent " + id + " Location " + location[0]+","+location[1]);
+		this.id = id;
+		this.color=color;
+		this.location=location;
+	}
+
+	public String act() {
+		
+		
+		//continued NoOp action to wait until other agents finish
+		if (this.plan.size()==0)
+			return "NoOp";
+		//test end
+		System.err.println(this.id+" action is !! "+this.plan.peek().toString());
+		return this.plan.poll().toString();
+	}
+
+	public void findMyGoals(List< Goal > goals){
+		for (int i = 0; i < goals.size(); i++){
+			Goal a_goal=goals.get(i);
+			int agent2goal=RandomWalkClient.level_grid.getBFSDistance(a_goal.location,this.location);
+			if(a_goal.color.equals(this.color) && agent2goal!=Integer.MAX_VALUE && !a_goal.claimed){
+				for(Box mybox:myBoxes.values()){
+					if(Character.toLowerCase(mybox.id)==a_goal.id){
+						myGoals.put(new Point(a_goal.location[0],a_goal.location[1]),a_goal);
+						a_goal.claimed=true;
+						
+					}
+				}
+			}
+		}
+	}
+	public void findMyBoxes(List< Box > boxes, HashMap<String,ArrayList<Agent>> color_agents){
+		ArrayList<Agent> same_color_agents=color_agents.get(this.color);
+		for (int i = 0; i < boxes.size(); i++){
+			Box a_box=boxes.get(i);
+			int agent2box=RandomWalkClient.level_grid.getBFSDistance(a_box.location,this.location);
+			if(a_box.color.equals(this.color) && agent2box!=Integer.MAX_VALUE && !a_box.claimed){
+				//TEST LOGIC, Load Balancer logic
+				int closest_agent=Integer.MAX_VALUE;
+				for (Agent same_color_agent: same_color_agents){
+					int other_agent2box=RandomWalkClient.level_grid.getBFSDistance(a_box.location,same_color_agent.location);
+					if(other_agent2box<closest_agent)
+						closest_agent=other_agent2box;
+				}
+				if (closest_agent==agent2box){
+					myBoxes.put(new Point(a_box.location[0],a_box.location[1]), a_box);
+					a_box.claimed=true;
+				}
+			}
+		}
+	}
+	public String printMyBoxes(){
+		String s="";
+		for(Box mybox : myBoxes.values()){
+			s+=mybox.id+"\n";
+		}
+		System.err.println("my ID: "+id+" my boxes: "+s);
+		return s;
+	}
+	public String printMyGoals(){
+		String s="";
+		for(Goal mygoal : myGoals.values()){
+			s+=mygoal.id+"\n";
+		}
+		System.err.println("my ID: "+id+" my goals: "+s);
+		return s;
+	} 
+	
+	
+	
+	public int createPlan(){ 
+		System.err.println("MY ID is: "+this.id);
+		System.err.println("MY BOX KEYS:"+myBoxes.keySet().toString());
+		System.err.println("MY GOAL KEYS:"+myGoals.keySet().toString());
+		
+		while(!myBoxes.keySet().containsAll(myGoals.keySet()))
+		{
+			//System.err.println("MY BOX VALUES:"+myBoxes.values().toString());
+			int currentDist=Integer.MAX_VALUE;
+			for (Goal a_goal : myGoals.values()){
+				
+				Point a_goal_loc = new Point(a_goal.location[0],a_goal.location[1]);
+				if(myBoxes.containsKey(a_goal_loc)){
+					continue;
+				}
+				
+				for(Box a_box : myBoxes.values()){
+					
+					Point a_box_loc = new Point(a_box.location[0],a_box.location[1]);
+					if(myGoals.containsKey(a_box_loc)){
+						continue;
+					}					
+					
+					int dist1= RandomWalkClient.level_grid.getBFSDistance(a_box.location,a_goal.location);
+					int dist2= RandomWalkClient.level_grid.getBFSDistance(a_box.location,this.location);
+					if(dist1!=0 && dist1+dist2 < currentDist && Character.toUpperCase(a_goal.id)==a_box.id){
+						currentDist = dist1+dist2;
+						currentGoal = a_goal;
+						currentBox = a_box;
+					}
+					
+				}
+			
+			}
+			//System.err.println("Agent at: "+this.location[0]+","+this.location[1]+" The box is"+currentBox.location[0]+","+currentBox.location[1]+"; Goal is "+currentGoal.location[0]+","+currentGoal.location[1]);
+			this.initialState = new Node(null,RandomWalkClient.all_walls, myBoxes,myGoals,currentBox,currentGoal, this.location);
+			this.strategy= new StrategyBestFirst(new AStar(initialState));
+			LinkedList<Node> singlesolution=new LinkedList<Node>();
+			
+			try {			
+				singlesolution=SearchSingle(strategy);
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.err.println("Exception happened, solution cannot be found!!");
+				e.printStackTrace();
+			}
+			this.solution.addAll(singlesolution);
+		}
+		return this.solution.size();
+		
+		
+		
+		
+		
+		//Old logic using A*//
+//		this.initialState= new Node(null,RandomWalkClient.all_walls, myBoxes,myGoals,this.location);
+//		strategy= new StrategyBestFirst(new AStar(initialState));
+//		try {
+//			this.solution=Search(strategy);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			System.err.println("Exception happened, solution cannot be found!!");
+//			e.printStackTrace();
+//		}
+//		
+//
+//		return this.solution.size();
+	}
+	
+	//Astar logic
+	public LinkedList<Node> Search(Strategy strategy) throws IOException {
+		System.err.format("Search starting with strategy %s.\n", strategy.toString());
+		strategy.addToFrontier(this.initialState);
+		
+		int iterations = 0;
+		while (true) {
+            if (iterations == 1000) {
+				System.err.println(strategy.searchStatus());
+				iterations = 0;
+			}
+
+			if (strategy.frontierIsEmpty()) {
+				return null;
+			}
+
+			Node leafNode = strategy.getAndRemoveLeaf();
+			if (leafNode.isGoalState()) {
+				return leafNode.extractPlan();
+			}
+
+			strategy.addToExplored(leafNode);
+			for (Node n : leafNode.getExpandedNodes()) { // The list of expanded nodes is shuffled randomly; see Node.java.
+				if (!strategy.isExplored(n) && !strategy.inFrontier(n)) {					
+					strategy.addToFrontier(n);
+				}
+			}
+			iterations++;
+		}
+	}
+	
+	//Single Astar logic
+	public LinkedList<Node> SearchSingle(Strategy strategy) throws IOException {
+		System.err.format("Search starting with strategy Single %s.\n", strategy.toString());
+		strategy.addToFrontier(this.initialState);
+		
+		int iterations = 0;
+		while (true) {
+            if (iterations == 1000) {
+				System.err.println(strategy.searchStatus());
+				iterations = 0;
+			}
+
+			if (strategy.frontierIsEmpty()) {
+				System.err.println("Frontier is Empty Now. Weird?");
+				return null;
+			}
+
+			Node leafNode = strategy.getAndRemoveLeaf();
+			if (leafNode.isSingleGoalState()) {
+				this.location=new int[]{leafNode.agentRow,leafNode.agentCol};
+				this.myBoxes=leafNode.boxes;
+				//update box locations for next subplan
+				for (Point box_loc : this.myBoxes.keySet())
+					this.myBoxes.get(box_loc).location=new int[]{box_loc.x,box_loc.y};
+				
+				return leafNode.extractPlan();
+			}
+
+			strategy.addToExplored(leafNode);
+			for (Node n : leafNode.getExpandedNodes()) { // The list of expanded nodes is shuffled randomly; see Node.java.
+				if (!strategy.isExplored(n) && !strategy.inFrontier(n)) {					
+					strategy.addToFrontier(n);
+				}
+			}
+			iterations++;
+		}
+	}
+
+}
+
+/*****************************************************************************/
+
+//goal class
+class Goal{
+	char id;
+	String color; //kept for finding links between box and goal
+	int[] location;
+	boolean claimed;
+	public Goal( char id, String color , int[] location) {
+		System.err.println("Found " + color + " Goal " + id + " Location " + location[0]+","+location[1]);
+		this.id = id;
+		this.color=color;
+		this.location=location;
+		this.claimed=false;
+	}
+	@Override
+	public boolean equals(Object o) {
+		Goal c = (Goal) o;
+        return c.id == id && c.color == color && Arrays.equals(c.location, location);
+    }
+    
+	@Override
+    public int hashCode() {
+    	return ((location[0] + location[1])*(location[0] + location[1] + 1))/2 + location[1];
+    }
+	@Override
+    public String toString(){
+    	return location[0]+","+location[1];
+    }
+}
+
+/*****************************************************************************/	
+//box class
+
+class Box{
+	char id;
+	String color;
+	int[] location;
+	boolean claimed;
+	public Box( char id, String color , int[] location) {
+		//System.err.println("Found " + color + " Box " + id + " Location " + location[0]+","+location[1]);
+		this.id = id;
+		this.color=color;
+		this.location=location;
+		this.claimed=false;
+	}
+	
+    
+	@Override
+	public boolean equals(Object o) {
+		Box c = (Box) o;
+        return c.id == id && c.color == color && Arrays.equals(c.location, location);
+    }
+    
+	@Override
+    public int hashCode() {
+    	return ((location[0] + location[1])*(location[0] + location[1] + 1))/2 + location[1];
+    }
+	 
+	@Override
+    public String toString(){
+    	return location[0]+","+location[1];
+    }
+}
+
---
+++ Mohammad/Final project/Merged/MergedProject/src/sampleclients/RandomWalkClient.java
@@ -0,0 +1,354 @@
+package sampleclients;
+
+import java.awt.Point;
+import java.io.*;
+import java.util.*;
+
+import sampleclients.Command.dir;
+import sampleclients.Command.type;
+import sampleclients.Strategy;
+import sampleclients.Heuristic.AStar;
+import sampleclients.Strategy.StrategyBestFirst;
+import sampleclients.Vertex;
+import sampleclients.Node;
+import sampleclients.Agent;
+
+public class RandomWalkClient {
+	private static Random rand = new Random();
+	private BufferedReader in = new BufferedReader( new InputStreamReader( System.in ) );
+	public TreeMap<Character, Agent > all_agents = new TreeMap<Character, Agent >();
+	public HashMap<String,ArrayList<Agent>> color_agents=new HashMap<String,ArrayList<Agent>>();
+	public List< Goal > all_goals = new ArrayList< Goal >();
+	public List< Box > all_boxes = new ArrayList< Box >();
+	public HashMap<Integer,Vertex> dij_graph=new HashMap<Integer,Vertex>();
+	public static boolean[][] all_walls=null;
+	boolean[][] all_frees=null;
+	public static Grid level_grid=null; //this is the grid to save all distance between any pair of locations 
+		
+	
+	public List<String> conflicts = null;
+	HashMap<Integer, ArrayList<Integer>> pathPositions = new HashMap<Integer, ArrayList<Integer>>();
+	
+	public RandomWalkClient() throws IOException {
+		readMap();
+	}
+
+	private void readMap() throws IOException {
+		Map< Character, String > colors = new HashMap< Character, String >();		
+		String line, color;
+		
+		//compute the dimension of the level file in the first place.
+		int[] dimension=this.getLevelDimension(in);
+		all_walls = new boolean[dimension[0]][dimension[1]];
+		all_frees = new boolean[dimension[0]][dimension[1]]; //pesudo free cell, everywhere other than walls
+
+		
+		/*****************************Read In the Level Layout************************************/
+		// Read lines specifying colors
+		int lineN=0;
+		while ( ( line = in.readLine() ).matches( "^[a-z]+:\\s*[0-9A-Z](,\\s*[0-9A-Z])*\\s*$" ) ) {
+			line = line.replaceAll( "\\s", "" );
+			color = line.split( ":" )[0];
+
+			for ( String id : line.split( ":" )[1].split( "," ) ){
+				colors.put( id.charAt( 0 ), color );
+			}
+		}
+
+		
+		
+		// Read lines specifying level layout
+		while ( !line.equals( "" ) ) {
+			for ( int i = 0; i < line.length(); i++ ) {
+				char id = line.charAt( i );
+				
+				//if the chr is a wall
+				if (id == '+') { // Wall.
+					all_walls[lineN][i] = true; //lineN is the row, i is the column
+				}
+				
+				//if the chr is an agent
+				else if ( '0' <= id && id <= '9' ){
+					System.err.println("Found agent "+id);
+					Agent new_agent =new Agent(id, colors.get(id),new int[]{lineN,i});
+					all_agents.put(id,new_agent);
+					
+					//categorize agents by color
+					if(!color_agents.containsKey(new_agent.color))
+						color_agents.put(new_agent.color, new ArrayList<Agent>());					
+					color_agents.get(new_agent.color).add(new_agent);
+					//	
+					
+					all_frees[lineN][i] = true;
+				}
+				
+				//save all goals
+				else if('a' <= id && id <= 'z'){
+					System.err.println("Found Goal "+id);
+					all_goals.add(new Goal(id, colors.get(Character.toUpperCase(id)),new int[]{lineN,i}));
+					all_frees[lineN][i] = true;
+				}
+				
+				//save all boxes
+				else if ('A' <= id && id <= 'Z'){
+					System.err.println("Found box "+id);
+					all_boxes.add(new Box(id, colors.get(id),new int[]{lineN,i}));
+					all_frees[lineN][i] = true;
+				}else if (id == ' ') {
+					all_frees[lineN][i] = true;
+				}else{
+					System.err.println("Error, read invalid level character: " + (int) i);
+					System.exit(1);
+				}
+			}
+
+			
+			line = in.readLine();
+			lineN++;
+			
+		}
+		/*******************************************************************************************/
+		
+
+		
+		/*****************************Create Grid BFS Distance Heuristic****************************/
+		createDistanceMap(); // creat distance map between every pair of locations in the level file.
+		//System.err.println("**************** Start this bxtch ************************");
+		level_grid=new Grid(this.dij_graph);
+		
+		/******************************************************************************************/
+		
+		
+		
+		/*****************************Create Plan For Each Legal Agent***********************************/
+		for (Character agent_id : all_agents.keySet()){
+			Agent a_agent=all_agents.get(agent_id);
+			a_agent.findMyBoxes(all_boxes,color_agents);
+			//a_agent.printMyBoxes();
+			a_agent.findMyGoals(all_goals);
+			//a_agent.printMyGoals();
+			System.err.println("Create plan for agent "+a_agent.id+ ": with length="+a_agent.createPlan());
+			
+		}
+		
+		/******************************************************************************************/
+		
+		detectConflicts();
+		
+	
+	}
+	
+	public void computePaths() {
+		// Returns all cell positions in agent's path
+		pathPositions.clear();
+		
+		for (Character agentID : all_agents.keySet()){
+			//int agent_id = Integer.toString(agentID).charAt(0);
+			int agent_id = Character.getNumericValue(agentID);
+			
+			Agent getAgent = all_agents.get(agentID);
+			int locX = getAgent.initialState.agentCol;
+			int locY = getAgent.initialState.agentRow;
+			
+			//System.err.println("AGENT " + agent_id + ", INITIAL LOCATION: " +  + locX + ", " + locY);
+			
+			pathPositions.put(agent_id, new ArrayList<Integer>());
+			pathPositions.get(agent_id).add(locX);
+			pathPositions.get(agent_id).add(locY);
+			
+			getAgent.plan.clear();
+			
+			for(int i = 0; i < getAgent.solution.size(); i++) {
+				getAgent.plan.add(getAgent.solution.get(i).action);
+			}			
+			
+			ListIterator<Command> listIterator = getAgent.plan.listIterator();
+			while (listIterator.hasNext()) {
+				Command getAction = listIterator.next(); 
+				System.err.println("AGENT: " + agent_id + ", DIR1: " + getAction.dir1 + ", DIR2: " + getAction.dir2);
+				//System.err.println("AGENT: " + agent_id + ", LOCATION: (" + locX + ", " + locY + ")");
+	
+				//System.err.println("PATHPOS X: " + pathPositions.get(agent_id).get(pathPositions.get(agent_id).size() - 1) + ", PATHPOS Y: " + pathPositions.get(agent_id).get(pathPositions.get(agent_id).size() - 2));
+				
+				switch(getAction.dir1.toString()) {
+					case "N":
+						locY = pathPositions.get(agent_id).get(pathPositions.get(agent_id).size() - 1) - 1;
+						break;
+					case "S":
+						locY = pathPositions.get(agent_id).get(pathPositions.get(agent_id).size() - 1) + 1;
+						break;
+					case "W":
+						locX = pathPositions.get(agent_id).get(pathPositions.get(agent_id).size() - 2) - 1;
+						break;
+					case "E":
+						locX = pathPositions.get(agent_id).get(pathPositions.get(agent_id).size() - 2) + 1;
+						break;
+					default:
+						locY = pathPositions.get(agent_id).get(pathPositions.get(agent_id).size() - 1);
+						locX = pathPositions.get(agent_id).get(pathPositions.get(agent_id).size() - 2);
+				}
+				
+				pathPositions.get(agent_id).add(locX);
+				pathPositions.get(agent_id).add(locY);
+				
+			}
+			
+			System.err.println("PATHS: " + pathPositions.get(agent_id));
+			
+		}
+		
+	}
+	
+	public void detectConflicts() {
+		// Loop through lists to detect conflicts.
+		computePaths();
+		
+		for(int i = 0; i < all_agents.size() - 1; i++) {
+			for(int j = i+1; j < all_agents.size(); j++) {
+				//char charI = Integer.toString(i).charAt(0);
+				//char charJ = Integer.toString(j).charAt(0);
+				
+				System.err.println("AGENT " + i + ", " + pathPositions.get(i).size());
+				System.err.println("AGENT " + j + ", " + pathPositions.get(j).size());
+				
+				for(int k = 0; k < Math.min(pathPositions.get(i).size(), pathPositions.get(j).size()); k++) {
+					
+					try {
+						if((pathPositions.get(i).get(k).equals(pathPositions.get(j).get(k))) && (pathPositions.get(i).get(k+1).equals(pathPositions.get(j).get(k+1))) ||
+								(pathPositions.get(i).get(k).equals(pathPositions.get(j).get(k+2))) && (pathPositions.get(i).get(k+1).equals(pathPositions.get(j).get(k+3))) ||
+								(pathPositions.get(i).get(k+2).equals(pathPositions.get(j).get(k))) && (pathPositions.get(i).get(k+3).equals(pathPositions.get(j).get(k+1)))) {
+							System.err.println("CONFLICT DETECTED BETWEEN AGENTS: " + i + " & " + j + ", @ (" + pathPositions.get(j).get(k) + "," + pathPositions.get(j).get(k+1) + ") AT INDEX " + (k + 1) / 2);
+							System.err.println("COMMAND:" + Command.every[0].toString());
+							all_agents.get(Character.forDigit(j, 10)).plan.add((k + 1) / 2 - 1, new Command());
+							all_agents.get(Character.forDigit(j, 10)).plan.add((k + 1) / 2 - 1, new Command());
+							break;
+						}
+						
+						// TO DO:
+						// - Detect boxes (2 NoOps, replan after)
+						// - Detect non-moving agents
+						// - 
+						
+						
+						k++;
+					} catch(Exception e) {
+						// OUT OF BOUNDS
+					}
+				}
+
+				
+			}
+			
+			System.err.println("NEW PLAN: " + all_agents.get(Character.forDigit(i, 10)).plan.toString());
+		}
+		
+	}
+	
+	
+	private String type(Command command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean update() throws IOException {
+		String jointAction = "[";
+
+		for (Character agent_id : all_agents.keySet()) {
+			jointAction += all_agents.get( agent_id ).act() + ",";
+		}
+		
+		jointAction = jointAction.substring(0,jointAction.length()-1)  + "]";
+
+		// Place message in buffer
+		System.out.println( jointAction );
+		
+		// Flush buffer
+		System.out.flush();
+
+		// Disregard these for now, but read or the server stalls when its output buffer gets filled!
+		String percepts = in.readLine();
+		System.err.println( "percepts: "+ percepts );
+		
+		if ( percepts == null )
+			return false;
+
+		return true;
+	}
+	
+	public void createDistanceMap(){
+		//Put all free cells into a map
+		for (int frow = 1; frow < all_frees.length-1; frow++) {
+			for (int fcol = 1; fcol < all_frees[0].length-1; fcol++) {
+				if(all_frees[frow][fcol]){
+					//do dijkstra mapping below
+					Vertex dj_vertex= new Vertex(frow,fcol);
+					//four directions
+					if(!dij_graph.containsKey(dj_vertex.hashCode())){
+						dij_graph.put(dj_vertex.hashCode(), dj_vertex);	
+						Vertex dj_adj_vertex;
+						if (all_frees[frow-1][fcol]){
+							dj_adj_vertex = new Vertex(frow-1,fcol);
+							dj_vertex.setEdge(dj_adj_vertex);
+							//dij_graph.get(dj_vertex).add(dj_adj_vertex);
+						}
+						if (all_frees[frow+1][fcol]){
+							dj_adj_vertex = new Vertex(frow+1,fcol);
+							dj_vertex.setEdge(dj_adj_vertex);
+							//dij_graphdj_adj_vertex);
+						}
+						if (all_frees[frow][fcol-1]){
+							dj_adj_vertex = new Vertex(frow,fcol-1);
+							dj_vertex.setEdge(dj_adj_vertex);
+							//dij_graph.get(dj_vertex).add(dj_adj_vertex);
+						}
+						if (all_frees[frow][fcol+1]){
+							dj_adj_vertex = new Vertex(frow,fcol+1);
+							dj_vertex.setEdge(dj_adj_vertex);
+						}
+						
+										
+					}
+				
+				}
+			}
+		}
+				
+	}
+	
+	public int[] getLevelDimension(BufferedReader serverMessages) throws IOException{
+		int longest=0, row=0, line_length=0;
+		serverMessages.mark(10000);
+		String line = serverMessages.readLine();
+		while (!line.equals("")) {
+			line = serverMessages.readLine();
+			line_length=line.length();
+			if (line_length>=longest){
+				longest=line_length;
+			}
+			row++;
+		}
+		int[] dimension={row,longest};
+		serverMessages.reset();
+		return dimension;	
+	}
+	
+
+	public static void main( String[] args ) {
+
+		// Use stderr to print to console
+		System.err.println( "Hello from RandomWalkClient. I am sending this using the error outputstream" );
+		for (int i=0; i < Command.every.length;i ++){
+			System.err.println( Command.every[i]);
+		}
+		try {
+			RandomWalkClient client = new RandomWalkClient();
+			int counter=0;
+			while ( client.update() ){
+				counter++;
+			}
+		System.err.println( "Updates:"+ counter );
+		} catch ( IOException e ) {
+			// Got nowhere to write to probably
+		}
+	}
+}
